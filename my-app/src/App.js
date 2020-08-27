4763787e6aeee892b519a0f6ca754eb5544eb08b
ddsolanki <dhaval.d.solanki@gmail.com>
2020-08-27 11:17:59 +0530

    first sample

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,23 +2,24 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const employees = [{ id: 1, name: 'John', desig: 'SE', salary: 10000 }, { id: 2, name: 'Doe', desig: 'SSE', salary: 15000 }];
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table className="emp-table">
+        <tr>
+          <th>Name</th>
+          <th>Desig</th>
+          <th>Salary</th>
+        </tr>
+        {employees.map((emp) => ( 
+          <tr>
+            <td>{emp.name}</td>
+            <td>{emp.desig}</td>
+            <td>{emp.salary}</td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }
