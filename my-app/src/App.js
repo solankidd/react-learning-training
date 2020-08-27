8ea16ca151d0dda101919bf616b57f9468de827c
ddsolanki <dhaval.d.solanki@gmail.com>
2020-08-27 15:49:19 +0530

    Porper styling to header and table

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,32 +2,46 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import Header from './components/header/Header'
+import { render } from 'react-dom';
 
 function App() {
   let employees = [{ id: 1, name: 'John', desig: 'SE', salary: 10000 }, { id: 2, name: 'Doe', desig: 'SSE', salary: 15000 }];
+
+
+  function renderTableHeader() {
+    let header = Object.keys(employees[0]);
+    return header.map((key, index) => {
+      return <th key={index}>{key.toUpperCase()}</th>
+   })
+  }
+
+  function renderTableData() {
+    return employees.map((emp) => ( 
+      <tr key={emp.id}>
+        <td>{emp.id}</td>
+        <td>{emp.name}</td>
+        <td>{emp.desig}</td>
+        <td>{emp.salary}</td>
+      </tr>
+    ))
+  };
+
   return (
     <div className="App">
       <Header/>
       <table className="table">
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Desig</th>
-            <th>Salary</th>
+            {renderTableHeader()}
           </tr>
         </thead>
         <tbody>
-          {employees.map((emp) => ( 
-            <tr key={emp.id}>
-              <td>{emp.name}</td>
-              <td>{emp.desig}</td>
-              <td>{emp.salary}</td>
-            </tr>
-          ))}
+          {renderTableData()}
         </tbody>
       </table>
     </div>
   );
+  
 }
 
 export default App;
--- my-app/src/components/header/Header.js
+++ my-app/src/components/header/Header.js
@@ -3,7 +3,7 @@ import './Header.scss';
 
 const Header= ()=> {
     return (
-        <header>
+        <header className="main-header">
             Cybage
         </header>
     )
